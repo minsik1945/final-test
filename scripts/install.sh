40937f2265c05935268a8a5b8553e5c2e1db3a3b
EC2 Default User <ec2-user@ip-172-31-11-82.ap-northeast-2.compute.internal>
2024-07-09 04:03:20 +0000

    re-commit

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+
 sudo yum -y install npm
 sudo yum -y install httpd
-sudo cat << EOF >> /etc/httpd/conf/httpd.conf
-DocumentRoot "/home/ec2-user/startbootstrap-shop-homepage/dist"
-<Directory "/home/ec2-user/startbootstrap-shop-homepage/dist">
-    Options Indexes FollowSymLinks
-    AllowOverride None
-    Require all granted
-</Directory>
-EOF
+sudo yum -y install php php-cli php-common php-fpm php-xml php-mysqlnd php-gd php-curl php-json php-mbstring php-zip php-intl
+
+
+HTTPD_FILE="/home/ec2-user/startbootstrap-shop-homepage/scripts/config_httpd"
+
+# A 파일의 내용을 변수에 저장
+HTTPD_CONTENT=$(cat "$HTTPD_FILE")
+
+# /etc/httpd/conf/httpd.conf 파일에 HTTPD 파일의 내용을 덮어쓰기
+echo "$HTTPD_CONTENT" | sudo tee /etc/httpd/conf/httpd.conf > /dev/null
+
+sudo systemctl restart httpd
+
